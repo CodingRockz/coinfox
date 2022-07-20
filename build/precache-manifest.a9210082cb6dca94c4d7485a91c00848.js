d8733d8b8d5e134d6b3bfbe2faee4950378b002b
CodingRockz <108911973+CodingRockz@users.noreply.github.com>
2022-07-20 18:25:55 +0800

    Add files via upload

---
+++ build/precache-manifest.a9210082cb6dca94c4d7485a91c00848.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d523689cd42df8f202899d95112ad5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6297c630bccee31ea43f",
+    "url": "/static/css/2.dd69c3c8.chunk.css"
+  },
+  {
+    "revision": "b5bdc68981e22015a678",
+    "url": "/static/css/main.ce0813bc.chunk.css"
+  },
+  {
+    "revision": "6297c630bccee31ea43f",
+    "url": "/static/js/2.bd3e6c70.chunk.js"
+  },
+  {
+    "revision": "927cd0571ab902b02e94cc7164e770be",
+    "url": "/static/js/2.bd3e6c70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5bdc68981e22015a678",
+    "url": "/static/js/main.ab5af153.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ab5af153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397ec531fae9f18a35dd",
+    "url": "/static/js/runtime-main.a3031cf3.js"
+  }
+]);
